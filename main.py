0bc83a22ed4e7b344bd552e9f01bd548477090ce
sakonakouma <62636801+sakonakouma@users.noreply.github.com>
2020-05-23 13:39:51 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+data = pd.read_csv("Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
+data.head()
+data.tail()
+import datetime
+end_date=datetime.datetime(2017, 2, 1)
+start_date=datetime.datetime(2010, 1, 1)
+
+num_month=(end_date.year-start_date.year)*12+(end_date.month-start_date.month)+1
+
+print("Total month:", num_month)
